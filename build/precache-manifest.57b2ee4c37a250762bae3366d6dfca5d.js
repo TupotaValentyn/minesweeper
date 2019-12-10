acd270dcea2b1cb45a17b56e8a8a31c4f751e9c4
TupotaValentyn <gm.tupota.valik@gmail.com>
2019-12-11 01:40:31 +0200

    Add build version

---
+++ build/precache-manifest.57b2ee4c37a250762bae3366d6dfca5d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8991dc77b8893e33c9857cfa278e8665",
+    "url": "./index.html"
+  },
+  {
+    "revision": "dcb945bf3efbfebfab1f",
+    "url": "./static/css/main.60dea729.chunk.css"
+  },
+  {
+    "revision": "015d1605f3550a70032a",
+    "url": "./static/js/2.6f1a77a1.chunk.js"
+  },
+  {
+    "revision": "dcb945bf3efbfebfab1f",
+    "url": "./static/js/main.361d9452.chunk.js"
+  },
+  {
+    "revision": "17dd016adcac7bdc2962",
+    "url": "./static/js/runtime-main.0a145108.js"
+  }
+]);
